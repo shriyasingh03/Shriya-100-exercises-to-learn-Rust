2acbb1a827320665175687c2a7bc5ac92037ac52
Shriya Singh <2001shriya@gmail.com>
2026-04-17 12:49:09 +0530

    added the exercises folder-> 01_intro folder

---
+++ exercises/01_intro/syntax_01/target/debug/deps/syntax_01-9fa252d363520822.d
@@ -0,0 +1,5 @@
+D:\Rust\100 exercises in rust\Shriya-100-exercises-to-learn-Rust\exercises\01_intro\syntax_01\target\debug\deps\syntax_01-9fa252d363520822.d: src\main.rs
+
+D:\Rust\100 exercises in rust\Shriya-100-exercises-to-learn-Rust\exercises\01_intro\syntax_01\target\debug\deps\syntax_01-9fa252d363520822.exe: src\main.rs
+
+src\main.rs:
---
+++ exercises/01_intro/welcome_00/target/debug/deps/welcome_00-92cde243e5c727df.d
@@ -0,0 +1,5 @@
+D:\Rust\100 exercises in rust\Shriya-100-exercises-to-learn-Rust\exercises\01_intro\welcome_00\target\debug\deps\welcome_00-92cde243e5c727df.d: src\main.rs
+
+D:\Rust\100 exercises in rust\Shriya-100-exercises-to-learn-Rust\exercises\01_intro\welcome_00\target\debug\deps\welcome_00-92cde243e5c727df.exe: src\main.rs
+
+src\main.rs:
