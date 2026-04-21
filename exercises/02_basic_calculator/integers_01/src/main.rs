98cb951e5ca442d4cc195cc51c2cf0a09f709eae
Shriya Singh <2001shriya@gmail.com>
2026-04-21 19:44:59 +0530

    Added 01_integer cargo crate

---
+++ exercises/02_basic_calculator/integers_01/src/main.rs
@@ -0,0 +1,17 @@
+// use std::parse;
+
+fn compute(a: u32, b: u32) -> u32 {
+    // TODO: change the line below to fix the compiler error and make the tests pass.
+    let multiplier: u8 = 4;
+    a + b * (multiplier as u32)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::compute;
+
+    #[test]
+    fn case() {
+        assert_eq!(compute(1, 2), 9);
+    }
+}
---
+++ exercises/02_basic_calculator/integers_01/target/debug/deps/integers_01-6833ba4734b80ea5.d
@@ -0,0 +1,5 @@
+D:\Rust\100 exercises in rust\Shriya-100-exercises-to-learn-Rust\exercises\02_basic_calculator\integers_01\target\debug\deps\integers_01-6833ba4734b80ea5.d: src\main.rs
+
+D:\Rust\100 exercises in rust\Shriya-100-exercises-to-learn-Rust\exercises\02_basic_calculator\integers_01\target\debug\deps\integers_01-6833ba4734b80ea5.exe: src\main.rs
+
+src\main.rs:
