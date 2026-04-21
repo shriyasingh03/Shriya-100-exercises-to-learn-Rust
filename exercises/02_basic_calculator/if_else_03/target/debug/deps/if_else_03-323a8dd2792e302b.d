4bd6ca24e5c5a996d3aaa1ea96bdfe7054838b89
Shriya Singh <2001shriya@gmail.com>
2026-04-21 20:25:39 +0530

    Added solution of 03_if_else

---
+++ exercises/02_basic_calculator/if_else_03/target/debug/deps/if_else_03-323a8dd2792e302b.d
@@ -0,0 +1,5 @@
+D:\Rust\100 exercises in rust\Shriya-100-exercises-to-learn-Rust\exercises\02_basic_calculator\if_else_03\target\debug\deps\if_else_03-323a8dd2792e302b.d: src\main.rs
+
+D:\Rust\100 exercises in rust\Shriya-100-exercises-to-learn-Rust\exercises\02_basic_calculator\if_else_03\target\debug\deps\if_else_03-323a8dd2792e302b.exe: src\main.rs
+
+src\main.rs:
