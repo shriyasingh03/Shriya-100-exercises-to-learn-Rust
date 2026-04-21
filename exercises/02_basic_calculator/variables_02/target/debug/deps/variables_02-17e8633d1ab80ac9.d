760a9d78c59ef68d0445eaffb45bae43a993a47e
Shriya Singh <113788850+shriyasingh03@users.noreply.github.com>
2026-04-21 19:39:14 +0530

    Delete exercises/02_basic_calculator directory

--- exercises/02_basic_calculator/variables_02/target/debug/deps/variables_02-17e8633d1ab80ac9.d
+++
@@ -1,5 +0,0 @@
-D:\Rust\100 exercises in rust\Shriya-100-exercises-to-learn-Rust\exercises\02_basic_calculator\variables_02\target\debug\deps\variables_02-17e8633d1ab80ac9.d: src\main.rs
-
-D:\Rust\100 exercises in rust\Shriya-100-exercises-to-learn-Rust\exercises\02_basic_calculator\variables_02\target\debug\deps\variables_02-17e8633d1ab80ac9.exe: src\main.rs
-
-src\main.rs:
