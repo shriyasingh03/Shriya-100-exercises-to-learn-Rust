38f754a7af53b2c852568931b4a5f1564b7001ef
Shriya Singh <2001shriya@gmail.com>
2026-04-21 20:35:30 +0530

    solution of 04_panics

---
+++ exercises/02_basic_calculator/panics_04/target/debug/deps/panics_04-5f488bfcff5fe7ce.d
@@ -0,0 +1,5 @@
+D:\Rust\100 exercises in rust\Shriya-100-exercises-to-learn-Rust\exercises\02_basic_calculator\panics_04\target\debug\deps\panics_04-5f488bfcff5fe7ce.d: src\main.rs
+
+D:\Rust\100 exercises in rust\Shriya-100-exercises-to-learn-Rust\exercises\02_basic_calculator\panics_04\target\debug\deps\panics_04-5f488bfcff5fe7ce.exe: src\main.rs
+
+src\main.rs:
