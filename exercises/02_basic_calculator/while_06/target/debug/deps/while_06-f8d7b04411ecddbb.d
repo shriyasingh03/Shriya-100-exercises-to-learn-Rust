9bafd22fc64c6d9a7c2c7bf7d54fc9b290314a9a
Shriya Singh <2001shriya@gmail.com>
2026-04-22 15:43:19 +0530

    solution to 06_while

---
+++ exercises/02_basic_calculator/while_06/target/debug/deps/while_06-f8d7b04411ecddbb.d
@@ -0,0 +1,5 @@
+D:\Rust\100 exercises in rust\Shriya-100-exercises-to-learn-Rust\exercises\02_basic_calculator\while_06\target\debug\deps\while_06-f8d7b04411ecddbb.d: src\main.rs
+
+D:\Rust\100 exercises in rust\Shriya-100-exercises-to-learn-Rust\exercises\02_basic_calculator\while_06\target\debug\deps\while_06-f8d7b04411ecddbb.exe: src\main.rs
+
+src\main.rs:
