c8d06470319fa13273b5d85f2d39d1ec8f7aa16c
Shriya Singh <2001shriya@gmail.com>
2026-04-22 17:28:32 +0530

    In the Cargo.toml in root repo , in dev portfolio - added overflow-checks = false

---
+++ exercises/02_basic_calculator/overflow_08/target/debug/deps/overflow_08-4c631c2abdda18e2.d
@@ -0,0 +1,5 @@
+D:\Rust\100 exercises in rust\Shriya-100-exercises-to-learn-Rust\exercises\02_basic_calculator\overflow_08\target\debug\deps\overflow_08-4c631c2abdda18e2.d: src\main.rs
+
+D:\Rust\100 exercises in rust\Shriya-100-exercises-to-learn-Rust\exercises\02_basic_calculator\overflow_08\target\debug\deps\overflow_08-4c631c2abdda18e2.exe: src\main.rs
+
+src\main.rs:
