328c2bfaf673916fa2c8ff1f78da934c5d0c7798
Shriya Singh <2001shriya@gmail.com>
2026-04-22 16:28:26 +0530

    solution to 07_for

---
+++ exercises/02_basic_calculator/for_07/src/main.rs
@@ -0,0 +1,46 @@
+// Rewrite the factorial function using a `for` loop.
+pub fn factorial(n: u32) -> u32 {
+let mut result = 1;
+
+
+    for  i in 1..=n  {
+        result *= i;
+    }
+    result
+
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::factorial;
+
+    #[test]
+    fn first() {
+        assert_eq!(factorial(0), 1);
+    }
+
+    #[test]
+    fn second() {
+        assert_eq!(factorial(1), 1);
+    }
+
+    #[test]
+    fn third() {
+        assert_eq!(factorial(2), 2);
+    }
+
+    #[test]
+    fn fifth() {
+        assert_eq!(factorial(5), 120);
+    }
+
+     #[test]
+    fn six() {
+        assert_eq!(factorial(6), 720);
+    }
+
+     #[test]
+    fn eight() {
+        assert_eq!(factorial(8), 	40320);
+    }
+}
---
+++ exercises/02_basic_calculator/for_07/target/debug/deps/for_07-4322913e40a1e695.d
@@ -0,0 +1,5 @@
+D:\Rust\100 exercises in rust\Shriya-100-exercises-to-learn-Rust\exercises\02_basic_calculator\for_07\target\debug\deps\for_07-4322913e40a1e695.d: src\main.rs
+
+D:\Rust\100 exercises in rust\Shriya-100-exercises-to-learn-Rust\exercises\02_basic_calculator\for_07\target\debug\deps\for_07-4322913e40a1e695.exe: src\main.rs
+
+src\main.rs:
