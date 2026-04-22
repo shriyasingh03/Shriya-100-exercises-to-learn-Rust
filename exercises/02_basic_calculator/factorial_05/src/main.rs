623eb21e5ea00912075e39e98651d5888d198358
Shriya Singh <2001shriya@gmail.com>
2026-04-22 15:29:40 +0530

    solution to 05_factorial

---
+++ exercises/02_basic_calculator/factorial_05/src/main.rs
@@ -0,0 +1,57 @@
+
+// Define a function named `factorial` that, given a non-negative integer `n`,
+// returns `n!`, the factorial of `n`.
+//
+// The factorial of `n` is defined as the product of all positive integers up to `n`.
+// For example, `5!` (read "five factorial") is `5 * 4 * 3 * 2 * 1`, which is `120`.
+// `0!` is defined to be `1`.
+//
+// We expect `factorial(0)` to return `1`, `factorial(1)` to return `1`,
+// `factorial(2)` to return `2`, and so on.
+//
+// Use only what you learned! No loops yet, so you'll have to use recursion!
+
+
+ pub fn factorial(integer: u64)-> u64{
+    //base case 
+  if integer ==0 || integer ==1 {
+    return 1;
+  }
+  else{
+    let result =   integer* factorial(integer -1 );
+    return result;
+  }  
+
+
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::factorial;
+
+    #[test]
+    fn first() {
+        assert_eq!(factorial(0), 1);
+    }
+
+    #[test]
+    fn second() {
+        assert_eq!(factorial(1), 1);
+    }
+
+    #[test]
+    fn third() {
+        assert_eq!(factorial(2), 2);
+    }
+
+    #[test]
+    fn fifth() {
+        assert_eq!(factorial(5), 120);
+    }
+
+    #[test]
+    fn six(){
+        assert_eq!(factorial(10),3628800);
+    }
+}
---
+++ exercises/02_basic_calculator/factorial_05/target/debug/deps/factorial_05-cb8425949a9ceb2f.d
@@ -0,0 +1,5 @@
+D:\Rust\100 exercises in rust\Shriya-100-exercises-to-learn-Rust\exercises\02_basic_calculator\factorial_05\target\debug\deps\factorial_05-cb8425949a9ceb2f.d: src\main.rs
+
+D:\Rust\100 exercises in rust\Shriya-100-exercises-to-learn-Rust\exercises\02_basic_calculator\factorial_05\target\debug\deps\factorial_05-cb8425949a9ceb2f.exe: src\main.rs
+
+src\main.rs:
