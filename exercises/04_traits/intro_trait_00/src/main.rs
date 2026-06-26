ca3b645e37bd21469554646476b099c64d86fbe5
Shriya Singh <2001shriya@gmail.com>
2026-06-26 20:43:41 +0530

    Added the 00_intro solution

---
+++ exercises/04_traits/intro_trait_00/src/main.rs
@@ -0,0 +1,15 @@
+#[allow(dead_code)]
+fn intro() -> &'static str {
+    // TODO: fix me 👇
+    "I'm ready to learn about traits!"
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::intro;
+
+    #[test]
+    fn test_intro() {
+        assert_eq!(intro(), "I'm ready to learn about traits!");
+    }
+}
