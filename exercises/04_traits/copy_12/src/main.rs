28e1a8ec8f9447d158b1b4621ad04a7f890dcfc7
sony <2001shriya@gmail.com>
2026-07-16 13:01:21 +0530

    Added solution of clone_12

---
+++ exercises/04_traits/copy_12/src/main.rs
@@ -0,0 +1,36 @@
+// TODO: implement the necessary traits to make the test compile and pass.
+//  You *can't* modify the test.
+use std::ops::Add;
+
+#[derive(Clone, Copy,Debug,PartialEq)]
+pub struct WrappingU32 {
+    value: u32,
+}
+
+impl WrappingU32 {
+    pub fn new(value: u32) -> Self {
+        Self { value }
+    }
+}
+
+impl Add for WrappingU32{
+    type Output = Self;
+    fn add(self, rhs:Self)->Self::Output{
+        Self { 
+            value: self.value.wrapping_add(rhs.value),
+         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ops() {
+        let x = WrappingU32::new(42);
+        let y = WrappingU32::new(31);
+        let z = WrappingU32::new(u32::MAX);
+        assert_eq!(x + y + y + z, WrappingU32::new(103));
+    }
+}
