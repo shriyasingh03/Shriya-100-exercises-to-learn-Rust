e8ff4fdf35ce1ca98afe379d5950096c2329f2b8
sony <2001shriya@gmail.com>
2026-07-17 11:36:41 +0530

    Solution of 00_intro

---
+++ exercises/05_ticket_v2/intro_00_v2/src/main.rs
@@ -0,0 +1,14 @@
+pub fn intro() -> &'static str {
+    // TODO: fix me 👇
+    "I'm ready to refine the `Ticket` type!"
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::intro;
+
+    #[test]
+    fn test_intro() {
+        assert_eq!(intro(), "I'm ready to refine the `Ticket` type!");
+    }
+}
